6e80a9748ffc16d3209fd1af9170e42678de15ab
Natan Menegasse <natan.menegasse@hotmail.com>
2025-03-10 23:01:16 -0300

    Built in builder

---
+++ chaosBuild.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+	"time"
+)
+
+var cmd bytes.Buffer
+
+func touchFile(name string) (*time.Time, error) {
+	file, err := os.Stat(name)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to touch file %s becuase of %s\n", name, err.Error())
+		return nil, err
+	}
+	stat := file.Sys().(*syscall.Stat_t)
+	var ctime time.Time = time.Unix(int64(stat.Ctim.Sec), int64(stat.Ctim.Nsec))
+	return &ctime, nil
+}
+
+func goRebuildYourSelfTek() {
+	buildSourcePath := "./chaosBuild.go"
+	timeSrc, err := touchFile(buildSourcePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to touch file %s because of %s\n", buildSourcePath, err.Error())
+		os.Exit(1)
+	}
+
+	buildBinPath := "./chaosBuild"
+	timeBin, err := touchFile(buildBinPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to touch file %s because of %s\n", buildSourcePath, err.Error())
+		os.Exit(1)
+	}
+
+	if (*timeSrc).Compare(*timeBin) == 1 {
+		if _, err := os.Stat(buildBinPath); err != nil {
+			fmt.Printf("File %s not found. Skipping renaming\n", buildBinPath)
+		} else {
+			fmt.Printf("Renaming %s to %s.old\n", buildBinPath, buildBinPath)
+			// Todo: why I need new line here?
+			oldFile := fmt.Sprintf("%s.old\n", buildBinPath)
+			os.Rename(buildBinPath, oldFile)
+			os.Remove(oldFile)
+		}
+		
+		cmd.WriteString(fmt.Sprintf("go build %s", buildSourcePath))
+		defer cmd.Reset()
+		if _, err := exec.Command("bash", "-c", cmd.String()).Output(); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to recompile %s because of %s\n", buildSourcePath, err.Error())
+			os.Exit(1)
+		}
+		goRebuildYourSelfTek()
+	}
+}
+
+func main() {
+
+	goRebuildYourSelfTek()
+	build_dir := "./build"
+	if _, err := os.Stat(build_dir); err != nil {
+		if err := os.Mkdir(build_dir, os.FileMode(0777)); err != nil {
+			fmt.Fprintf(os.Stderr, "Filed to create directory %s because of %s\n", build_dir, err.Error())
+			os.Exit(1)
+		}
+	}
+
+	main := "./src/main.go"
+	cmd.WriteString(fmt.Sprintf("go build -o ./build/main %s ", main))
+	defer cmd.Reset()
+	if _, err := exec.Command("bash", "-c", cmd.String()).Output(); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to compile %s because of %s\n", main, err.Error())
+		os.Exit(1)
+	}
+}
+
