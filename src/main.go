2a67a30f9e0ec7225f0000aee7feffef04cf06c4
Natan Menegasse <natan.menegasse@hotmail.com>
2025-03-13 23:51:30 -0300

    Improved the tokenizer and created a simple parser

--- src/main.go
+++ src/main.go
@@ -6,13 +6,68 @@ import (
 	"os"
 	"strings"
 	"unicode"
+	"strconv"
 )
 
 type TokenType int
 
+const (
+	tokGlobal TokenType = iota
+	tokLet
+	tokAssignment
+	tokPlus
+	tokType
+	tokVariable
+	tokNumber
+	tokSemiColon
+	tokCount
+)
+
+type TokenKind struct {
+	unsignedInt64 bool
+}
+
 type Token struct {
 	value   string
 	tokType TokenType
+	tokKind TokenKind
+}
+
+func tokTypeToString(tokType2 TokenType) string {
+	switch tokType2 {
+	case tokGlobal:
+		return "tokGlobal"
+	case tokLet:
+		return "tokLet"
+	case tokAssignment:
+		return "tokAssingment"
+	case tokPlus:
+		return "tokPlus"
+	case tokType:
+		return "tokType"
+	case tokVariable:
+		return "tokVariable"
+	case tokNumber:
+		return "tokNumber"
+	case tokSemiColon:
+		return "tokSemiColon"
+	}
+	fmt.Fprintf(os.Stderr, "[ERROR] We did not expect this tokType '%d' - please include a new case or fix you shitty code :)\n", tokType2)
+	os.Exit(1)
+	return "unrecheable"
+}
+
+func printTokens(listTok []Token) {
+
+	fmt.Printf("Token List:\n")
+	for _, theToken := range listTok {
+		fmt.Printf("  -> Token { value: '%s', type: '%s' }\n", theToken.value, tokTypeToString(theToken.tokType))
+	}
+}
+
+func isUppercase(b byte) bool {
+	s := rune(b)
+	return unicode.IsUpper(s) && unicode.IsLetter(s)
 }
 
 func isNum(b byte) bool {
@@ -40,7 +95,7 @@ func isInside(b byte, listChar []byte) bool {
 	return result
 }
 
-func chaosTokenizer(file *[]byte) error {
+func chaosTokenizer(file *[]byte) []Token {
 	tokens := bytes.Buffer{}
 	listTok := []Token{}
 	pos := 0
@@ -58,7 +113,6 @@ func chaosTokenizer(file *[]byte) error {
 			token := Token{
 				value: tokens.String(),
 			}
-			fmt.Printf("Found token: %s\n", token.value)
 			listTok = append(listTok, token)
 			tokens.Reset()
 			continue
@@ -70,7 +124,6 @@ func chaosTokenizer(file *[]byte) error {
 			token := Token{
 				value: tokens.String(),
 			}
-			fmt.Printf("Found token: %s\n", token.value)
 			listTok = append(listTok, token)
 			tokens.Reset()
 			continue
@@ -82,7 +135,6 @@ func chaosTokenizer(file *[]byte) error {
 			token := Token{
 				value: tokens.String(),
 			}
-			fmt.Printf("Found token: %s\n", token.value)
 			listTok = append(listTok, token)
 			tokens.Reset()
 			continue
@@ -91,7 +143,40 @@ func chaosTokenizer(file *[]byte) error {
 			os.Exit(1)
 		}
 	}
-	return nil
+	return listTok
+}
+
+func chaosParser(listTok []Token) []Token {
+	for id := range listTok {
+		_, err := strconv.Atoi(listTok[id].value)
+		if err == nil {
+			listTok[id].tokType = tokNumber
+			continue
+		}
+		if listTok[id].value == "global" {
+			listTok[id].tokType = tokGlobal
+			continue
+		} else if listTok[id].value == "let" {
+			listTok[id].tokType = tokLet
+			continue
+		} else if isUppercase(listTok[id].value[0]) {
+			listTok[id].tokType = tokType
+			continue
+		} else if listTok[id].value == "+" {
+			listTok[id].tokType = tokPlus
+			continue
+		} else if listTok[id].value == ";" {
+			listTok[id].tokType = tokSemiColon
+			continue
+		} else if listTok[id].value == "=" {
+			listTok[id].tokType = tokAssignment
+			continue
+		} else {
+			listTok[id].tokType = tokVariable
+			continue
+		}
+	}
+	return listTok
 }
 
 func main() {
@@ -114,7 +199,9 @@ func main() {
 				fmt.Fprintf(os.Stderr, "[ERROR] Failed to open the file for whatever reason\n")
 				os.Exit(1)
 			}
-			chaosTokenizer(&file)
+			listTok := chaosTokenizer(&file)
+			listTok = chaosParser(listTok)
+			printTokens(listTok)
 		} else {
 			fmt.Fprintf(os.Stderr, "[ERROR] Failed to the program %s\n", programName)
 			fmt.Printf("Usage: %s <file.chaos>\n", programName)
