50840b97e6210309a0e30d64fec25ab9266121cb
Natan Menegasse <natan.menegasse@hotmail.com>
2025-03-13 21:52:25 -0300

    Small adjustments to the tokenizer

--- src/main.go
+++ src/main.go
@@ -4,85 +4,101 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"unicode"
 )
 
 type TokenType int
-
-const (
-	tokGlobal TokenType = iota
-	tokLet
-	tokVariable
-	tokType
-	tokAssign
-	tokNum
-)
 
 type Token struct {
 	value   string
 	tokType TokenType
 }
 
+func isNum(b byte) bool {
+	s := rune(b)
+	return unicode.IsNumber(s)
+}
+
+func isAlpha(b byte) bool {
+	s := rune(b)
+	return unicode.IsLetter(s)
+}
+
+func isAlphanum(b byte) bool {
+	s := rune(b)
+	return unicode.IsNumber(s) || unicode.IsLetter(s)
+}
+
+func isInside(b byte, listChar []byte) bool {
+	result := false
+	for _, item := range listChar {
+		if result = (b == item); result {
+			break
+		}
+	}
+	return result
+}
+
 func parseFile(filePath string) error {
 	if _, err := os.Stat(filePath); err != nil {
-		fmt.Fprintf(os.Stderr, "The provided file path '%s' doesn't exist\n", filePath)
+		fmt.Fprintf(os.Stderr, "[ERROR] The provided file path '%s' doesn't exist\n", filePath)
 		return err
 	}
 	file, err := os.ReadFile(filePath)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to open the file for whatever reason\n")
+		fmt.Fprintf(os.Stderr, "[ERROR] Failed to open the file for whatever reason\n")
 		return err
 	}
 
 	tokens := bytes.Buffer{}
-	actualString := string(file)
-	size := len(actualString)
+	content := string(file)
 	listTok := []Token{}
-	id := 0
-	for {
-		if id == size {
-			break
+	pos := 0
+
+	singleTokens := []byte{'=', '+', ';'}
+
+	for pos < len(content) {
+		if unicode.IsSpace(rune(content[pos])) {
+			pos++
+			continue
+		} else if isInside(content[pos], singleTokens) {
+			tokens.WriteByte(content[pos])
+			pos++
+			token := Token{
+				value: tokens.String(),
+			}
+			fmt.Printf("Found token: %s\n", token.value)
+			listTok = append(listTok, token)
+			tokens.Reset()
+			continue
+		} else if isNum(content[pos]) {
+			for isNum(content[pos]) {
+				tokens.WriteByte(content[pos])
+				pos++
+			}
+			token := Token{
+				value: tokens.String(),
+			}
+			fmt.Printf("Found token: %s\n", token.value)
+			listTok = append(listTok, token)
+			tokens.Reset()
+			continue
+		} else if isAlpha(content[pos]) {
+			for isAlphanum(content[pos]) {
+				tokens.WriteByte(content[pos])
+				pos++
+			}
+			token := Token{
+				value: tokens.String(),
+			}
+			fmt.Printf("Found token: %s\n", token.value)
+			listTok = append(listTok, token)
+			tokens.Reset()
+			continue
+		} else {
+			fmt.Fprintf(os.Stderr, "[ERROR] Failed while tokenizing - unknown character '%s'\n", string(content[pos]))
+			os.Exit(1)
 		}
-		if actualString[id] == ' ' {
-			if tokens.String() == "global" {
-				id++
-				token := Token{
-					value:   tokens.String(),
-					tokType: tokGlobal,
-				}
-				listTok = append(listTok, token)
-				tokens.Reset()
-			}
-			if tokens.String() == "let" {
-				id++
-				token := Token{
-					value:   tokens.String(),
-					tokType: tokLet,
-				}
-				listTok = append(listTok, token)
-				tokens.Reset()
-			}
-			if 'a' < actualString[id] || actualString[id] > 'Z' {
-				variableBuf := bytes.Buffer{}
-				for {
-					fmt.Printf("%s\n", string(actualString[id]))
-					variableBuf.WriteByte(actualString[id])
-					if actualString[id] == ' ' {
-						id++
-						token := Token{
-							value:   variableBuf.String(),
-							tokType: tokVariable,
-						}
-						listTok = append(listTok, token)
-						variableBuf.Reset()
-						break
-					}
-					id++
-				}
-			}
-			fmt.Printf("%v\n", listTok)
-		}
-		tokens.WriteByte(actualString[id])
-		id++
 	}
 	return nil
 }
@@ -90,7 +106,7 @@ func parseFile(filePath string) error {
 func main() {
 	programName := os.Args[0]
 	if len(os.Args) <= 1 {
-		fmt.Fprintf(os.Stderr, "Failed to the program %s\n", programName)
+		fmt.Fprintf(os.Stderr, "[ERROR] Failed to the program %s\n", programName)
 		fmt.Printf("Usage: %s <file.chaos>\n", programName)
 		os.Exit(1)
 	}
