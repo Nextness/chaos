6008b9ffe5a6418c82b90cc01e64a404e4910967
Natan Menegasse <natan.menegasse@hotmail.com>
2025-05-21 21:17:25 -0300

    Including type checking for strings

--- src/typeCheckingChaos.go
+++ src/typeCheckingChaos.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
 )
 
 func typeCheckingChaos(es *ExecutionState) {
@@ -11,11 +11,21 @@ func typeCheckingChaos(es *ExecutionState) {
 	for _, node := range es.entryPoint.statements {
 		var typeMatches bool = true
 		if node, ok := cast[*NodeIdentifier](node); ok {
+
 			// RHS is a token literal
 			if token, ok := cast[*TokenLiteral](node.value); ok {
-				typeMatches = typeMatches && node.varType.tokKind.matchAt(0, kindU64, kindU32)
-				typeMatches = typeMatches && token.tokType == tokNumber
-				checked[node.identifier.symbol] = node
+				if _, ok := cast[int](token.value); ok {
+					typeMatches = typeMatches && node.varType.tokKind.matchAt(0, kindU64, kindU32)
+					typeMatches = typeMatches && token.tokType == tokNumber
+					checked[node.identifier.symbol] = node
+				} else if _, ok := cast[string](token.value); ok {
+					typeMatches = typeMatches && node.varType.tokKind.matchAt(0, kindString)
+					typeMatches = typeMatches && token.tokType == tokString
+					checked[node.identifier.symbol] = node
+				} else {
+					panic(fmt.Sprintf("Unknown token type for token %+v\n", token))
+				}
+
 				if !typeMatches {
 					panic("Handle type checking error")
 				}
@@ -30,10 +40,18 @@ func typeCheckingChaos(es *ExecutionState) {
 				a := castAssert[*NodeIdentifier](checked[token.symbol])
 				b := castAssert[*TokenLiteral](a.value)
 
-				typeMatches = typeMatches && b.tokType == tokNumber
-				typeMatches = typeMatches && n.varType.tokKind == a.varType.tokKind
-				typeMatches = typeMatches && n.varType.tokKind.matchAt(0, kindU64, kindU32)
-				checked[node.identifier.symbol] = a
+				if _, ok := cast[int](b.value); ok {
+					typeMatches = typeMatches && b.tokType == tokNumber
+					typeMatches = typeMatches && n.varType.tokKind == a.varType.tokKind
+					typeMatches = typeMatches && n.varType.tokKind.matchAt(0, kindU64, kindU32)
+					checked[node.identifier.symbol] = a
+				} else if _, ok := cast[string](b.value); ok {
+					typeMatches = typeMatches && b.tokType == tokString
+					typeMatches = typeMatches && n.varType.tokKind == a.varType.tokKind
+					typeMatches = typeMatches && n.varType.tokKind.matchAt(0, kindString)
+					checked[node.identifier.symbol] = a
+				}
+
 				if !typeMatches {
 					panic("Handle type checking error")
 				}
@@ -42,4 +60,3 @@ func typeCheckingChaos(es *ExecutionState) {
 		}
 	}
 }
-
