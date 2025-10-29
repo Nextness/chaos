f44715e39a5027739e7751ab00d73110671dcb1b
Natan Menegasse <natan.menegasse@hotmail.com>
2025-10-28 21:32:50 -0300

    Removing unnecessary abstractions

--- src/chaosTypeInference.go
+++ src/chaosTypeInference.go
@@ -1,90 +1,75 @@
 package main
 
-import (
-	"fmt"
-)
-
-func matchVarDeclIdentifierSymbol(a any, b any, c ...bool) bool {
-	nodeA := castAssert[Node](a)
-	nodeB := castAssert[Node](b)
-
-	result := true
-	for _, v := range c {
-		result = result && v
-	}
-
-	return result && (nodeA.VarDecl.Name.Symbol == nodeB.VarDecl.Assignment.VarDecl.Name.Symbol)
-}
-
-func matchVarDeclLiteralSymbol(a any, b any, c ...bool) bool {
-	assert[any](len(c) == 0, "This filter doesn't expect more inputs")
-	nodeA := castAssert[Node](a)
-	nodeB := castAssert[Node](b)
-	return nodeA.VarDecl.Name.Symbol == nodeB.VarDecl.Name.Symbol
-}
-
-func MatchInScope(scope Scope, compare func(a any, b any, c ...bool) bool, b any, c ...bool) (bool, int) {
-	for idx, node := range scope {
-		if compare(node, b) {
-			return true, idx
-		}
-	}
-	return false, 0
-}
+import "fmt"
 
 func InferVarDeclType(program *ChaosSlice[Node], scope Scope) bool {
 	node := CSGetPointer(program)
 
 	if node.VarDecl.Assignment.NodeType == nodeIntLiteral {
-		match, idx := MatchInScope(scope, matchVarDeclLiteralSymbol, *node)
-		assert[any](match, "Cannot infer variable declaration since it doesn't exist in scope")
-		s64 := MakeType("S64")
-		node.VarDecl.Type = s64
-		scope[idx].VarDecl.Type = s64
-		return true
+		for idx, n := range scope {
+			if n.VarDecl.Name.Symbol == node.VarDecl.Name.Symbol {
+				s64 := MakeType("S64")
+				node.VarDecl.Type = s64
+				scope[idx].VarDecl.Type = s64
+				return true
+			}
+		}
+		assert[any](false, "Cannot infer variable declaration since it doesn't exist in scope")
 	}
 
 	if node.VarDecl.Assignment.NodeType == nodeStringLiteral {
-		match, idx := MatchInScope(scope, matchVarDeclLiteralSymbol, *node)
-		assert[any](match, "Cannot infer variable declaration since it doesn't exist in scope")
-		str := MakeType("String")
-		node.VarDecl.Type = str
-		scope[idx].VarDecl.Type = str
-		return true
+		for idx, n := range scope {
+			if n.VarDecl.Name.Symbol == node.VarDecl.Name.Symbol {
+				str := MakeType("String")
+				node.VarDecl.Type = str
+				scope[idx].VarDecl.Type = str
+				return true
+			}
+		}
+		assert[any](false, "Cannot infer variable declaration since it doesn't exist in scope")
 	}
 
 	if node.VarDecl.Assignment.NodeType == nodeBoolLiteral {
-		match, idx := MatchInScope(scope, matchVarDeclLiteralSymbol, *node)
-		assert[any](match, "Cannot infer variable declaration since it doesn't exist in scope")
-		boolean := MakeType("Bool")
-		node.VarDecl.Type = boolean
-		scope[idx].VarDecl.Type = boolean
-		return true
+		for idx, n := range scope {
+			if n.VarDecl.Name.Symbol == node.VarDecl.Name.Symbol {
+				boolean := MakeType("Bool")
+				node.VarDecl.Type = boolean
+				scope[idx].VarDecl.Type = boolean
+				return true
+			}
+		}
+		assert[any](false, "Cannot infer variable declaration since it doesn't exist in scope")
 	}
 
 	if node.VarDecl.Assignment.NodeType == nodeIdentifier {
-		match, idx := MatchInScope(scope, matchVarDeclIdentifierSymbol, *node, node.VarDecl.Assignment.NodeType == nodeIdentifier)
-		assert[any](match, "Cannot infer variable declaration since it doesn't exist in scope")
-		node.VarDecl.Type = scope[idx].VarDecl.Type
-		return true
+		for idx, n := range scope {
+			condition := (node.VarDecl.Assignment.NodeType == nodeIdentifier &&
+				n.VarDecl.Name.Symbol == node.VarDecl.Assignment.VarDecl.Name.Symbol)
+			if condition {
+				node.VarDecl.Type = scope[idx].VarDecl.Type
+				return true
+			}
+		}
+		assert[any](false, "Cannot infer variable declaration since it doesn't exist in scope")
 	}
 
-	// TO-DO: Handle proc calls
-	// if node.VarDecl.Assignment.NodeType == nodeCall {
-	// 	rhs := node.VarDecl.Assignment.Call
-	// 	for _, n := range scope {
-	// 		if n.NodeType == nodeIdentifier && rhs.Name.Symbol == n.VarDecl.Name.Symbol {
-	// 			returnsLen := len(n.VarDecl.Assignment.Proc.Outputs)
-	// 			assert[any](returnsLen <= 1, "Cannot have multiple returns for now")
-	// 			if returnsLen != 0 {
-	// 				node.VarDecl.Type = n.VarDecl.Assignment.Proc.Outputs[0].VarDecl.Type
-	// 			}
-	// 			break
-	// 		}
-	// 	}
-	// 	CSConsume(program)
-	// 	continue
-	// }
+	if node.VarDecl.Assignment.NodeType == nodeCall {
+		for _, n := range scope {
+			condition := (n.NodeType == nodeIdentifier &&
+				node.VarDecl.Assignment.Call.Name.Symbol == n.VarDecl.Name.Symbol)
+			if condition {
+				retLen := len(n.VarDecl.Assignment.Proc.Outputs)
+				assert[any](retLen <= 1, "Cannot have multiple returns for now")
+				node.VarDecl.Type = MakeType("Void")
+				if retLen == 1 {
+					node.VarDecl.Type = n.VarDecl.Assignment.Proc.Outputs[0].VarDecl.Type
+				}
+				return true
+			}
+		}
+		assert[any](false, "Cannot infer variable declaration since it doesn't exist in scope")
+	}
+
 	return false
 }
 
